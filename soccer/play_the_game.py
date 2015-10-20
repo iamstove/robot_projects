01f84ac67e2da20a840015b730068aad561a17d1
Steven Turner <sturner1123@gmail.com>
2015-10-20 14:06:29 -0400

    added odom to play the game for later use

--- soccer/play_the_game.py
+++ soccer/play_the_game.py
@@ -14,8 +14,25 @@ K_D = 1.25 # K_d in the PID equation
 
 color_namelist = ['Greenline', 'Redball', 'Orangegoal'] # We'll use this for indexing different colors.
 
+def odomCallback(data): #still think we might want to publish to speed control to move, but we'll still need odom for finding the angles
+    global del_x
+    global del_r
+    # Convert quaternion to degree
+    q = [data.pose.pose.orientation.x,
+         data.pose.pose.orientation.y,
+         data.pose.pose.orientation.z,
+         data.pose.pose.orientation.w]
+    roll, pitch, yaw = euler_from_quaternion(q)
+    # roll, pitch, and yaw are in radian
+    # degree = yaw * 180 / math.pi
+    del_x = data.pose.pose.position.x
+    del_r = yaw
+    y = data.pose.pose.position.y
+    #msg = "(%.6f,%.6f) at %.6f degree." % (x, y, degree)
+    #rospy.loginfo(msg)
+
 def blobsCallback(data): # This is called whenever a blobs message is posted; this happens constnatly even if blobs are not detected
-	global curr_blobweights 
+	global curr_blobweights
 	global has_new_blobinfo
 	x = [0, 0, 0] # Greenline, Redball, Orangegoal
 	y = [0, 0, 0] # could be generalized but is ok for now
@@ -38,7 +55,7 @@ def blobsCallback(data): # This is called whenever a blobs message is posted; th
 			else:
 				x[color_index] /= area[color_index]
 				y[color_index] /= area[color_index]
-			
+
 		curr_blobweights = [x, y] # Boom.
 		has_new_blobinfo = True
 
@@ -58,9 +75,9 @@ def blobsCallback(data): # This is called whenever a blobs message is posted; th
 def init_all():
 	twist_init()
 	global has_new_blobinfo
-	
+
 	has_new_blobinfo = False
-	
+
 def twist_init():
 	global curr_velocity
 	global pastloc
@@ -76,18 +93,18 @@ def follow_the_line():
 	global has_new_blobinfo
 	### This is similar to nodes in that it's basically waiting for a publish trigger to come down
 	### and signal this 'node' to get to work.
-	
+
 	# We will have a trigger that will mean we're finished with the line; this trigger requires we have not seen
 	### a green blob for 10 new sets of blobs.  This trigger activates with changes in not_done_with_line
 	not_done_with_line = True
-	
+
 	hope = 0 # The amount of hope we have at this point in time that we're on the line
-	
+
 	# The loop will incorporate a wait that waits 1/10 the time we waited to get the blob the last time.
 	### This is so the program doesn't consume huge CPU.
 	time_waited = 0 # We don't have to wait at all to try the first time.
 	time_last = time.clock() # Start the time so we know when we started later.
-	
+
 	while not_done_with_line:
 		while not(has_new_blobinfo):
 			rospy.sleep(time_waited / 10) # Sleep for a bit.  Maybe?
@@ -103,7 +120,7 @@ def follow_the_line():
 			pastloc = blobloc
 			#print("going!!")
 			pub.publish(curr_velocity)
-			hope = 10 # We're hopeful that we'll continue to see the line 
+			hope = 10 # We're hopeful that we'll continue to see the line
 		else: # decide whether to stay still or keep up hope
 			hope -= 1
 			if hope < -20:
@@ -114,28 +131,28 @@ def follow_the_line():
 				pub.publish(curr_velocity)
 			else:
 				# We've still got hope!!!
-	
-	# Now we're done with the line, so we need to look for the ball	
+
+	# Now we're done with the line, so we need to look for the ball
 
 def play_ball():
 	SEARCH_NONE, SEARCH_BALL, SEARCH_GOAL # Supposed to be a state thing, complete this line
-	
+
 	# We first want to declare our dependency upon the global blob area.
 	global curr_blobweights # Note well that this is effectively all blobsCallback changes when it runs.
 	# We will simulate nodelike behavior by looping until a flag has_new_blobinfo is set.
 	global has_new_blobinfo
-	
+
 	### NEEDS: TURN -PI/2 RAD ###
 
 	### THEN, WE SEARCH ###
-	
+
 	not_done_with_search = True # We begin our search now, in fact
 
 	hope = 0 # The amount of hope we have at this point in time that we're seeing the ball
-	
+
 	time_waited = 0 # We don't have to wait at all to try the first time.
 	time_last = time.clock() # Start the time so we know when we started later.
-	
+
 	while not_done_with_search:
 		while not(has_new_blobinfo):
 			rospy.sleep(time_waited / 10) # Sleep for a bit.  Maybe?
@@ -161,17 +178,19 @@ def play_ball():
 				pub.publish(curr_velocity)
 			else:
 				# We've still got hope!!!
-	
-	# Now we're done with the line, so we need to look for the ball	
+
+	# Now we're done with the line, so we need to look for the ball
 
 def play_game():
 	init_all() # Initialize everything (includes initialization of the twist for current motion)
 	rospy.init_node('soccer_player', anonymous = True) # Initialize this node
 	rospy.Subscriber('/blobs', Blobs, blobsCallback)
-	
+	rospy.Subscriber('/odom', Odometry, odomCallback)
+
+
 	follow_the_line()
 	play_ball()
-	
+
 	rospy.spin()
 
 if __name__ == '__main__':
