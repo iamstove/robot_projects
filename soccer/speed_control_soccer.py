b88087299a9c81d97a1b03c2ef30da012b73c663
Steven Turner <sturner1123@gmail.com>
2015-10-28 17:14:42 -0400

    removed a print in soccer speed

--- soccer/speed_control_soccer.py
+++ soccer/speed_control_soccer.py
@@ -26,7 +26,7 @@ def twist_init():
 
 def parse_command(data):
 	command_list = data.data.split(',')
-	sys.stderr.write('List of commands: ' + str(command_list)+'\n')
+	#sys.stderr.write('List of commands: ' + str(command_list)+'\n')
 	#for each command in command list, split it up and send it to the accelerator program
 	for c in command_list:
 		#print ("Command: " + str(c))
