4fe6e2d2a29d8cba924df4716499db4c92ca27be
Student <student@r2-d2.(none)>
2015-12-02 14:00:53 -0500

    Code to handle blobs begun

--- selfie/lemmietakeaselfie.py
+++ selfie/lemmietakeaselfie.py
@@ -8,6 +8,7 @@ from std_msgs.msg import Empty
 from std_msgs.msg import String
 import sys
 import math
+import time
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from cmvision.msg import Blobs, Blob
@@ -23,6 +24,10 @@ depthImage = Image()
 mid_height = 240
 pub = rospy.Publisher('kobuki_command', Twist, queue_size=10)
 color_namelist = ['Orange', 'Pink', 'Green', 'Blue'] # We'll use this for indexing different colors.
+
+def data_init():
+	global pastData
+	pastData = [] 
 
 def twist_init():
 	global curr_velocity
@@ -42,8 +47,9 @@ def depthCallback(data):
 
 def blobsCallback(data):
 	"""updates the blobs"""
-	global blobData, isBlobReady
+	global blobData, isBlobReady, blobTime
 	blobData = data
+	blobTime = time.clock()
 	isBlobReady = True
 
 def updateColorImage(data):
@@ -80,10 +86,28 @@ def parseBlobs(data)
 				x[color_index] /= area[color_index]
 				y[color_index] /= area[color_index]
 
-		curr_blobweights = [x, y] # Boom.
+		curr_blobweights = [x, y, area] # Boom.
 	else:
-		curr_blobweights = [area, area]
+		curr_blobweights = [area, area, area]
 	has_new_blobinfo = True
+
+def handleBlobs():
+	global curr_blobweights
+	if not curr_blobweights[0][2] == -1 and not curr_blobweights[0][3] == -1:
+		# check if the blue and green are nearby relative to their areas
+		xdif = curr_blobweights[0][3] - curr_blobweights[0][2]
+		ydif = curr_blobweights[1][3] - curr_blobweights[1][2]
+		distsq = xdif*xdif + ydif*ydif
+		if distsq < curr_blobweights[2][3] * 4:
+			# We have someone to follow
+			followPoint = (curr_blobweights[0][3] - xdif/2.0, curr_blobweights[1][3] - ydif/2.0)
+			hasFollow = True
+		
+def handleDistance(): 	# If it exists, this function collects Kinect's distance at followPoint and handles
+			# the twist response and verbal response to seeing different environmental factors,
+			# recording followPoint->Kinect data in pastData and looking at the kinect depth field
+			# to figure out if we should panic and stop
+			# XXX: Move code from randmov for this, maybe? probably?
 
 def selfie(image):
 	path = "./pictures"
@@ -104,26 +128,38 @@ def selfie(image):
 	rospy.sleep(1)
 	os.system('spd-say \"Smile\"')
 	rospy.sleep(.25)
-	cv2.imwrite(filename, color_image, [cv2.IMWRITE_JPEG_QUALITY, 100])
+	cv2.imwrite(filename, color_image, [cv2.IMWRITE_JPEG_QUALITY, 100])			
 
 def main():
+	global curr_velocity, blobData, blobTime isBlobReady, isColorImageReady, blobCopy, timeCopy
 	twist_init()
+	data_init()
 	rospy.init_node('selife_stalker', anonymous = True) # Initialize this node
 	rospy.Subscriber('/blobs', Blobs, blobsCallback)
 	rospy.Subscriber("/camera/depth/image", Image, depthCallback, queue_size=10)
+	#rospy.Subscriber(<whoever does color images>, Image, updateColorImage, queue_size=10)
 	bridge = CvBridge()
 
 	while not isDepthReady and not rospy.is_shutdown() and not isBlobReady and not isColorImageReady:
 		pass
 
 	while not rospy.is_shutdown():
-		blobsCopy = copy.deepcopy(blobsData) #copy of blobs so it doesn't change
 		try:
 			color_image = bridge.imgmsg_to_cv2(colorImage, "bgr8")
 		except CvBridgeError, e:
 			print e
+
 		#check blobs
-		#if follow blob
+		while not isBlobReady: # XXX: Fast loop. Slow it down?
+			pass
+		blobCopy = copy.deepcopy(blobData) #copy of blobs so it doesn't change
+		timeCopy = blobTime
+		parseBlobs(blobCopy)
+		handleBlobs()
+		isBlobReady = False # Finished processing this batch
+		
+		if nowFollowBlob:
+			pub.publish(curr_velocity)
 			#move forward, keeping the blob centered, adjusting speed with distance
 			#check for obsticles
 		#if picture blob
