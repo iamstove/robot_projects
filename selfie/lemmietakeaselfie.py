44f2d5984190ee8de69c28480120d1ecebaaeab5
Steven Turner <sturner1123@gmail.com>
2015-12-03 11:50:31 -0500

    added randmov stuff

--- selfie/lemmietakeaselfie.py
+++ selfie/lemmietakeaselfie.py
@@ -59,6 +59,23 @@ def updateColorImage(data):
 	colorImage = data
 	isColorImageReady = True
 
+def scanup(column):
+	"""returns false for too far and returns true for too close"""
+	colarr = []
+	for pixel in range(479,239,-1): #spotcheck up every 15px, compile into array, (this will be 16 in length)
+		offset = (pixel * depthData.step) + (column * 4)
+		(val,) = unpack('f', depthData.data[offset] + depthData.data[offset+1] + depthData.data[offset+2] + depthData.data[offset+3])
+		colarr.append(val)
+
+	for d in range(240):
+		if (d+1) == len(colarr): #check if our step will put us out of bounds
+			break
+		else:
+			if colarr[d] > 5 and math.isnan(colarr[d+1]): #this would be the case where our nan is far away
+				return False
+			elif colarr[d] < 1.5 and math.isnan(colarr[d+1]): #the case when we should stop and turn
+				return True
+
 def parseBlobs(data)
 	global curr_blobweights
 	global has_new_blobinfo
@@ -178,3 +195,54 @@ def main():
 
 if __name__ == '__main__':
 	main()
+
+
+
+
+"""def main():
+	twist_init()
+	global depthData, isDepthReady, curr_velocity
+	rospy.init_node('depth_example', anonymous=True)
+	rospy.Subscriber("/camera/depth/image", Image, depthCallback, queue_size=10)
+	#sys.stderr.write("hello1\n")
+
+
+	while not isDepthReady and not rospy.is_shutdown():
+		pass
+
+	while not rospy.is_shutdown() and keepMove:
+		step = depthData.step
+		#sys.stderr.write("step: " +str(step)+ "\n")
+		horzArr = []
+		#tot = 0
+		for pixel in range(0, 640, 20): #build an array of values across the center of the screen (20px width)
+			#sys.stderr.write(str(i) + "\n")
+			offset = (mid_height * step) + (pixel * 4)
+			#sys.stderr.write(str(offset)+"\n")
+			(val,) = unpack('f', depthData.data[offset] + depthData.data[offset+1] + depthData.data[offset+2] + depthData.data[offset+3])
+			horzArr.append(val)
+			#tot += val
+			#sys.stderr.write("Distance: " + str(depthValue[i]) + "\n")
+
+		#tot /= 32
+		#sys.stderr.write("Distance: " + str(depthValue) + "\n")
+		#sys.stderr.write("Avg: " + str(tot) + "\n")
+
+		i = 0 #contains which column we're on
+		for value in horzArr:
+			if math.isnan(value): #check our spotchecks for nans
+				scanup(i)
+			else: #we have a real number, we want to see if its less than 1
+				if value < 1:
+					#stop and turn until we don't see it anymore
+					still_turning = True
+					still_turning = turn_away(i)
+					if still_turning: #wait until it's no longer turning
+						print "Massssssive error"
+				else:
+					#keep moving
+					curr_velocity.linear.x = .25
+					pub.publish(curr_velocity)
+					#print(str(curr_velocity))
+
+			i += 20"""
